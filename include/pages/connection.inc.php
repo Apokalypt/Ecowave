58c77c7e066759214850f1e513d232c8f8b687f3
LADRAT Mattéo <apokalyptdev@gmail.com>
2020-12-04 20:21:18 +0100

    Bug fixed --- Background doesn't take all height --- Sometimes the bottom of the form is hidden by the menu --- Sometimes the last text is not visible with the image background

--- include/pages/connection.inc.php
+++ include/pages/connection.inc.php
@@ -19,7 +19,8 @@ if (isConnected()) {
 	}
 	
 	if (!oneIsEmpty($_POST, 'password', 'email')) {
-		if ((new UserManager(new MyPDO()))->connect($_POST['email'], encryptStringNoDecrypt($_POST['password']))) {
+		$success = (new UserManager(new MyPDO()))->connect($_POST['email'], encryptStringNoDecrypt($_POST['password']));
+		if ($success) {
             $_SESSION['user_mail'] = $_POST['email'];
             $_SESSION['user_password'] = $_POST['password'];
 			
@@ -38,23 +39,28 @@ if (isConnected()) {
 	}
 } ?>
 
-<div class="d-flex h-100 background">
+<div class="d-flex background" style="background-attachment: fixed; min-height: 100%">
     <div class="col-sm-10 col-md-8 col-lg-6 m-auto text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.4s">
         <img src="images/Logo.png" alt="Logo" class="col-5 col-md-4">
         <h2 class="title mb-0 text-light">ECOWAVE</h2>
-        <form action="" method="post">
-            <div class="col-8 mx-auto mt-3">
+        <form class="pb-5" action="" method="post">
+            <div class="col-9 mx-auto mt-3">
                 <label for="email" class="button text-light display-5">Email</label>
-                <input class="w-100 rounded-pill" type="email" required name="email" id="email">
+                <input class="w-100 rounded-pill" type="email" required name="email" id="email" value="<?php if (isset($success) && !$success) echo arrayValue($_POST, 'email') ?>">
             </div>
-            <div class="col-8 mx-auto mt-3">
+            <div class="col-9 mx-auto mt-3">
                 <label for="password" class="button text-light display-5"><?php echo getString("password")?></label>
                 <input class="w-100 rounded-pill" type="password" required name="password" id="password">
             </div>
+			<?php if (isset($success) && !$success) { ?>
+				<div class="col-9 mx-auto mt-2 alert alert-danger">
+					Une erreur est survenue lors de la tentative de connexion...<br /> Adresse e-mail ou mot de passe incorrect...
+				</div>
+			<?php } ?>
             <div class="col-8 mx-auto mt-4">
-                <input class="w-100 button rounded-pill" type="submit" name="connexion" id="connexion" value="<?php echo getString("connection")?>">
+                <input class="w-100 button rounded-pill shadow-sm" type="submit" name="connexion" id="connexion" value="<?php echo getString("connection")?>">
             </div>
-            <a href="?page=<?php echo EnumPages::Register . "&action=" . $_GET['action'] ?>" class="button text-light"><?php echo getString("createAccount")?></a>
+            <a href="?page=<?php echo EnumPages::Register . "&action=" . $_GET['action'] ?>" class="button text-light" style="text-shadow: 2px 2px 4px #000000;"><?php echo getString("createAccount")?></a>
         </form>
     </div>
 </div>
