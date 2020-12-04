873eb1377e268a118e8a384036f0fdce69de22ad
LADRAT Mattéo <apokalyptdev@gmail.com>
2020-12-04 05:50:39 +0100

    Bug fixed --- User id isn't pass for profile

--- include/mobile-nav.inc.php
+++ include/mobile-nav.inc.php
@@ -1,5 +1,10 @@
 <div id="menu" class="text-center justify-content-space-between">
     <a href="?page=home" class="button text-dark">Accueil</a>
     <a href="#" class="button text-dark">Session</a>
-    <a href="?page=profile" class="button text-dark">Profil</a>
+	<?php if (isConnected()) {
+		/** @var User $account */?>
+		<a href="?page=<?php echo EnumPages::Profile . '&id=' . $account->getUserId() ?>" class="button text-dark">Profil</a>
+    <?php } else { ?>
+		<a href="?page=<?php echo EnumPages::Connection ?>" class="button text-dark">Se connecter</a>
+    <?php } ?>
 </div>
