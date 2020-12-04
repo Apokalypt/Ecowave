1bd442bbab77bf45ab972b6490db3d1f0f69b39d
Jean Laurent <jeanz.laurent@gmail.com>
2020-12-04 02:47:26 +0100

    Bug fixed --- forms register and connection

--- include/pages/connection.inc.php
+++ include/pages/connection.inc.php
@@ -6,21 +6,19 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 } ?>
 
 <div class="d-flex h-100 background">
-    <div class="col-sm-10 col-md-8 col-lg-6 m-auto text-center w-100 wrap justify-content-center align-self-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.4s">
+    <div class="col-sm-10 col-md-8 col-lg-6 m-auto text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.4s">
         <img src="images/Logo.png" alt="Logo" class="col-5 col-md-4">
-        <h2 class="col-12 text-center title mb-0 text-light">ECOWAVE</h2>
+        <h2 class="title mb-0 text-light">ECOWAVE</h2>
         <form action="" method="post">
-            <div class="col-8 m-auto">
+            <div class="col-8 mx-auto mt-3">
                 <label for="email" class="button text-light display-5">Email</label>
                 <input class="w-100 rounded-pill" type="email" name="email" id="email">
             </div>
-            <br>
-            <div class="col-8 m-auto">
+            <div class="col-8 mx-auto mt-3">
                 <label for="password" class="button text-light display-5">Mot de passe</label>
                 <input class="w-100 rounded-pill" type="password" name="password" id="password">
             </div>
-            <br>
-            <div class="col-8 m-auto">
+            <div class="col-8 mx-auto mt-4">
                 <input class="w-100 button rounded-pill" type="submit" name="connexion" id="connexion" value="Connexion">
             </div>
             <a href="index.php?page=register" class="button text-light">ou créé ton compte ici</a>
--- include/pages/register.inc.php
+++ include/pages/register.inc.php
@@ -5,36 +5,30 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     exit();
 } ?>
 <div class="d-flex h-100 background">
-    <div class="col-12 col-md-10 m-auto text-center">
+    <div class="col-sm-10 col-md-8 col-lg-6 m-auto text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.4s">
         <img src="images/Logo.png" alt="Logo" class="col-5 col-md-4">
         <h2 class="title mb-0 text-light">ECOWAVE</h2>
-        <form action="??" method="post">
-            <div>
+        <form action="" method="post">
+            <div class="col-8 mx-auto mt-3">
                 <label for="pseudo" class="button display-5 text-light">Pseudo</label>
-                <br>
-                <input type="text" name="pseudo" id="pseudo" class="col-7 rounded-pill">
+                <input type="text" name="pseudo" id="pseudo" class="w-100 rounded-pill">
             </div>
-            <br>
-            <div>
+            <div class="col-8 mx-auto mt-3">
                 <label for="email" class="button display-5 text-light">Email</label>
-                <br>
-                <input type="text" name="email" id="email" class="col-7 rounded-pill">
+                <input type="text" name="email" id="email" class="w-100 rounded-pill">
             </div>
-            <br>
-            <div>
+            <div class="col-8 mx-auto mt-3">
                 <label for="mdp" class="button display-5 text-light">Mot de passe</label>
-                <br>
-                <input type="password" name="mdp" id="mdp" class="col-7 rounded-pill">
+                <input type="password" name="mdp" id="mdp" class="w-100 rounded-pill">
             </div>
-            <br>
-            <div>
+            <div class="col-8 mx-auto mt-3">
                 <label for="mdp_confirm" class="button display-5 text-light">Confirmation mot de passe</label>
-                <br>
-                <input type="password" name="mdp_confirm" id="mdp_confirm" class="col-7 rounded-pill">
+                <input type="password" name="mdp_confirm" id="mdp_confirm" class="w-100 rounded-pill">
             </div>
-            <br>
-            <input type="submit" value="Nous rejoindre" class="button col-7 rounded-pill">
+            <div class="col-8 mx-auto mt-4">
+                <input value="Nous rejoindre" class="button w-100 rounded-pill" type="submit" name="connexion" id="connexion">
+            </div>
+            <a href="index.php?page=connection" class="button text-dark">ou connecte-toi ici</a>
         </form>
-        <a href="index.php?page=connection" class="button text-dark">ou connecte-toi ici</a>
     </div>
 </div>
