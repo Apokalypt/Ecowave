31ca54462a0189c9ea05fa8227dfc6a42fe23091
LADRAT Mattéo <apokalyptdev@gmail.com>
2020-12-04 05:10:44 +0100

    New feature --- Member can see his profile (need an update to add last review)

--- include/pages/profile.inc.php
+++ include/pages/profile.inc.php
@@ -8,17 +8,21 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 if (!isConnected()) {
 	header('Location:?page=' . EnumPages::Connection . '&action=profil');
 	exit();
-} ?>
+}
+
+/** @var User $account */?>
 <div class="d-flex h-100 background">
     <div class="col-sm-10 col-md-8 col-lg-6 m-auto text-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.4s">
         <img src="images/Logo.png" alt="Logo" class="col-5 col-md-4">
         <h2 class="title mb-0 text-light">ECOWAVE</h2>
         <div class="rond m-auto"></div>
-        <h2 class="button text-light">Pseudo <span>(<a href="#" class="text display-6 text-light">Modifier profil</a>)</span></h2>
+        <h2 class="button text-light"><?php echo $account->getUserPseudo() == '' ?  '<i>Aucun pseudo</i>' : '<az>' . $account->getUserPseudo() . '</az>' ?> <span>(<a href="#" class="text display-6 text-light">Modifier profil</a>)</span></h2>
         <h2 class="button text-light">Description</h2>
-        <p class="text text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, eveniet iure molestiae qui sapiente vel veniam. Accusantium aliquam, aut eligendi, esse expedita fugit in ipsum itaque iure ullam unde vel.</p>
+        <p class="text text-light"><?php echo $account->getUserBiography() == '' ?  '<i>Aucune description</i>' : '<az>' . $account->getUserBiography() . '</az>' ?></p>
+        <!--
         <h2 class="button text-light">Dernier avis</h2>
         <p class="text text-light">Date / Lieu</p>
         <p class="text text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, eveniet iure molestiae qui sapiente vel veniam. Accusantium aliquam, aut eligendi, esse expedita fugit in ipsum itaque iure ullam unde vel.</p>
+    	-->
     </div>
 </div>
