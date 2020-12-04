4034b64e90e7568a9925c939bc0993547f0978be
Jean Laurent <jeanz.laurent@gmail.com>
2020-12-04 01:53:33 +0100

    New features --- Adding mobile connection ui

--- include/pages/connection.inc.php
+++ include/pages/connection.inc.php
@@ -7,11 +7,23 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
 
 <div class="d-flex h-100 background">
     <div class="w-100 wrap justify-content-center align-self-center wow fadeIn" data-wow-duration="1.3s" data-wow-delay="0.4s">
+        <img class="d-flex col-6 m-auto" src="images/Logo.png" alt="logo">
         <h2 class="col-12 text-center title mb-0 text-light">ECOWAVE</h2>
-        <h2 class="col-12 text-center normal text-light">#BALANCETONSPOT</h2>
-
-        <div class="row justify-content-center m-0">
-            <a class="button btn btn-primary mt-5 rounded-pill px-4 py-2" role="button">SURFEZ !</a>
-        </div>
+        <form action="" method="post">
+            <div class="col-8 m-auto">
+                <label for="email">Email</label>
+                <input class="w-100" type="email" name="email" id="email">
+            </div>
+            <br>
+            <div class="col-8 m-auto">
+                <label for="password">Mot de passe</label>
+                <input class="w-100" type="password" name="password" id="password">
+            </div>
+            <br>
+            <div class="col-8 m-auto">
+                <input class="w-100" type="submit" name="connexion" id="connexion" value="Connexion">
+                <label class="" for="connexion"><a href="#">ou créé ton compte ici</a></label>
+            </div>
+        </form>
     </div>
 </div>
